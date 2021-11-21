09a00bdbe0824fa2b80c6ff4b45668c425410dc9
Kelvin Pompey <kelvin.pompey@gmail.com>
2021-11-21 12:43:58 -0500

    Cleanup

--- App.tsx
+++ App.tsx
@@ -3,7 +3,6 @@ import { NativeBaseProvider } from "native-base";
 import Navigation from "./components/Navigation";
 import AuthProvider from "./features/auth/AuthContext";
 import { theme } from "./theme";
-import "react-native-url-polyfill/auto";
 
 const App = () => {
   return (
